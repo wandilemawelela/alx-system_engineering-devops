fd363f493f22f84cffb9d12d2a2d73cf9a4549f0
Wandile Mawelela <wandile1997@gmail.com>
2024-03-06 08:41:55 +0200

    [FIX] Hot fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Uses the Reddit API to find the number of subs from a sub reddit. """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    """ Returns the number of subscribers for a given subreddit. """
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
